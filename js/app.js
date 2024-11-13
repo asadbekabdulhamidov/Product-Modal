ff8f50c59bb3f893ba20cc19d2c1b1a7851591a6
Asadbek <aabdulhamidov818@gmail.com>
2024-11-13 09:11:11 +0500

    product modal js DOM Project

---
+++ js/app.js
@@ -0,0 +1,283 @@
+const product = [
+  {
+    id: 0,
+    name: "red-chili",
+    mainImg: "./image/png/chili-red.png",
+    imgs: ["./dyehfe", "./vdsghfds"],
+    desc: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam, blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar. ",
+    price: 1,
+  },
+  {
+    id: 2,
+    name: "Big Potatos",
+    mainImg: "./image/png/potato.png",
+    imgs: ["./dyehfe", "./vdsghfds"],
+    desc: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam, blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar. ",
+    price: 9,
+  },
+  {
+    id: 3,
+    name: "Chanise Cabbage",
+    mainImg: "./image/png/green-cucumber.png",
+    imgs: ["./dyehfe", "./vdsghfds"],
+    desc: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam, blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar. ",
+    price: 7,
+  },
+  {
+    id: 4,
+    name: "Ladies Finger",
+    mainImg: "./image/png/finger.png",
+    imgs: ["./dyehfe", "./vdsghfds"],
+    desc: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam, blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar. ",
+    price: 8.5,
+  },
+  {
+    id: 5,
+    name: "Red Tomato",
+    mainImg: "./image/png/red-tomato.png",
+    imgs: ["./dyehfe", "./vdsghfds"],
+    desc: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam, blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar. ",
+    price: 3.3,
+  },
+  {
+    id: 6,
+    name: "Eggplant",
+    mainImg: "./image/png/eggplant.png",
+    imgs: ["./dyehfe", "./vdsghfds"],
+    desc: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam, blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar. ",
+    price: 1.99,
+  },
+  {
+    id: 7,
+    name: "Fresh Cauliflower",
+    mainImg: "./image/png/couliflower.png",
+    imgs: ["./dyehfe", "./vdsghfds"],
+    desc: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam, blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar. ",
+    price: 2.77,
+  },
+  {
+    id: 8,
+    name: "Green Apple",
+    mainImg: "./image/png/green-apple.png",
+    imgs: ["./dyehfe", "./vdsghfds"],
+    desc: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam, blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar. ",
+    price: 4.25,
+  },
+  {
+    id: 9,
+    name: "Fresh Mango",
+    mainImg: "./image/png/mango.png",
+    imgs: ["./dyehfe", "./vdsghfds"],
+    desc: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam, blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar. ",
+    price: 9.56,
+  },
+  {
+    id: 10,
+    name: "Green Capsicum",
+    mainImg: "./image/png/green-capsicum.png",
+    imgs: ["./dyehfe", "./vdsghfds"],
+    desc: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam, blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar. ",
+    price: 1.1,
+  },
+  {
+    id: 11,
+    name: "Green chili",
+    mainImg: "./image/png/green-chili.png",
+    imgs: ["./dyehfe", "./vdsghfds"],
+    desc: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam, blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar. ",
+    price: 5.89,
+  },
+  {
+    id: 12,
+    name: "Green Cucumber",
+    mainImg: "./image/png/green-cucumber.png",
+    imgs: ["./dyehfe", "./vdsghfds"],
+    desc: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam, blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar. ",
+    price: 7.23,
+  },
+  {
+    id: 13,
+    name: "Green Littuce",
+    mainImg: "./image/png/littuce.png",
+    imgs: ["./dyehfe", "./vdsghfds"],
+    desc: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam, blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar. ",
+    price: 3.2,
+  },
+  {
+    id: 14,
+    name: "Green Littuce",
+    mainImg: "./image/png/littuce2.png",
+    imgs: ["./dyehfe", "./vdsghfds"],
+    desc: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam, blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar. ",
+    price: 8.12,
+  },
+  {
+    id: 15,
+    name: "Ladies Finger",
+    mainImg: "./image/png/ladies finger.png",
+    imgs: ["./dyehfe", "./vdsghfds"],
+    desc: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam, blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar. ",
+    price: 6.12,
+  },
+  {
+    id: 16,
+    name: "Red Capsicum",
+    mainImg: "./image/png/red-capsicum.png",
+    imgs: ["./dyehfe", "./vdsghfds"],
+    desc: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam, blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar. ",
+    price: 4.5,
+  },
+];
+
+const cards=document.querySelector('.cards__container')
+const modalWrapper=document.querySelector('.modal__wrapper')
+const close=document.querySelector('.close')
+
+const [minus,plus]=document.querySelectorAll('.modal__count button')
+
+
+// const modalImg=document.querySelector('.modal-image')
+// const modalPrice=document.querySelector('.price')
+// const modalDesc=document.querySelector('.modal__desc-text')
+
+
+
+product.forEach((item)=>{
+    
+cards.innerHTML+=`
+    <div onclick="showModal(${item.id})" class="card">
+            <img src="${item.mainImg}" alt="${item.name}">
+            <div class="card__intro">
+                <p class="card-name">${item.name}</p>
+                <p class="product__price">
+                $
+                ${item.price}
+                </p>
+
+                <img src="./image/svg/Rating.svg" alt="rating">
+                <span class="bag-shopping">
+                <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M6.66667 8.83333H4.16667L2.5 18H17.5L15.8333 8.83333H13.3333M6.66667 8.83333V6.33333C6.66667 4.49239 8.15905 3 10 3V3C11.8409 3 13.3333 4.49238 13.3333 6.33333V8.83333M6.66667 8.83333H13.3333M6.66667 8.83333V11.3333M13.3333 8.83333V11.3333" stroke="#1A1A1A" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                </span>
+            </div>
+     </div>
+
+`
+}
+)
+
+function showModal(id){
+    const oneProduct=product.filter((item)=>{
+        return item.id===id
+    })
+    // console.log(oneProduct);
+    
+    modalWrapper.innerHTML=`
+       <div class="modal">
+        <div onclick="closeModal()" class="close">
+          <i class="fa-solid fa-xmark"></i>
+        </div>
+        <div class="left__imgs">
+          <i class="fa-solid fa-chevron-up"></i>
+          <img src="${oneProduct[0].mainImg}" alt="product img" class="img1">
+          <img src="${oneProduct[0].mainImg}" alt="product img" class="img4">
+          <img src="${oneProduct[0].mainImg}" alt="product img" class="img3">
+          <img src="${oneProduct[0].mainImg}" alt="product img" class="img2">
+          <i class="fa-solid fa-chevron-down"></i>
+        </div>
+        <img class="modal-image" src="${oneProduct[0].mainImg}" alt="main modal image">
+
+       <div class="modal__intro-wrapper">
+        <div class="modal__intro">
+          <h2 class="modal__title">${oneProduct[0].name} <span>In Stock</span></h2>
+          <div class="rating__wrap"><img src="./image/modal-image/Rating.png" alt=""> <b>SKU: <span>2,51,594</span></b></div>
+          <div class="product__price">
+            <span>$48.00</span>
+            <h2 class="price">$${oneProduct[0].price}</h2>
+            <p class="price__skidka">64% off</p>
+          </div>
+        </div>
+
+        <!-- modal description -->
+
+        <div class="modal__desc">
+          <div class="modal__desc-top">
+            <h3>Brand</h3>
+          <img src="./image/modal-image/product__modal.png" alt="">
+
+          <div class="socials">
+           <h4>Share item:</h4>
+            <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+            <a href="#">
+              <i class="fa-brands fa-pinterest"></i>
+            </a>
+            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+          </div>
+          </div>
+          <p class="modal__desc-text">
+            ${oneProduct[0].desc} 
+          </p>
+        </div>
+
+        <!-- mdoal btns -->
+
+        <div class="modal__btns">
+          <div class="modal__count">
+            <button onclick="minusCount()">-</button>
+            <p>5</p>
+            <button onclick="plusCount()">+</button>
+          </div>
+
+          <button class="add__btn">Add to Cart <i class="fa-solid fa-bag-shopping"></i></button>
+
+          <span class="heart__wrap"><i class="fa-regular fa-heart"></i></span>
+        </div>
+
+        <div class="category">
+          <h2>Category:
+            <p>Vegetables</p>
+          </h2>
+          <p>Tag:
+            Vegetables
+            Healthy
+            Chinese
+            Cabbage
+            Green Cabbage</p>
+        </div>
+       </div>
+       </div>
+    
+    `
+
+    modalWrapper.classList.remove('hidden')
+
+
+    
+}
+
+function closeModal(){
+    modalWrapper.classList.add('hidden')
+}
+
+function minusCount(){
+    const countProduct=document.querySelector('.modal__count p')
+    
+    if (parseInt(countProduct.textContent) > 0) {
+        countProduct.textContent = parseInt(countProduct.textContent) - 1;
+    }
+    
+}
+function plusCount(){
+    const countProduct=document.querySelector('.modal__count p')
+    countProduct.textContent = parseInt(countProduct.textContent) + 1;
+  
+}
+
+
+document.addEventListener('keydown',(e)=>{
+    if(e.key=='Escape') closeModal()
+})
+
+
